b095e029bd8f385507366ae6d23ba0ad6ee141b3
jamesmccabe <james.mccab3@gmail.com>
2018-09-27 01:39:40 +0100

    Updates

--- vol.lisp
+++ vol.lisp
@@ -1,15 +1,20 @@
 ;;; vol.lisp --- Controlling StumpWM volume on Ubuntu 18.04
 
+(setf *max-vol* 100)
+
 (defcommand volume-increase () ()
-  "Increase Volume"
-  (run-shell-command "pactl set-sink-volume 0 +5%"))
+  "Increase volume but not over 100%"
+  (setf *volume* (run-shell-command "pactl list sinks | grep -i volume | head -n1 | awk '{print $5}' | tr -d '\\r\\n'" t))
+  (if (>= (parse-integer *volume* :junk-allowed t) *max-vol*)
+      (message "Volume is at Maximum 100%!")
+    (run-shell-command "pactl set-sink-volume 0 +5%")))
 
 (defcommand volume-decrease () ()
-  "Decrease Volume"
+  "Decrease volume"
   (run-shell-command "pactl set-sink-volume 0 -5%"))
 
 (defcommand toggle-mute () ()
-  "Mute Volume"
+  "Mute volume"
   (run-shell-command "pactl set-sink-mute 0 toggle"))
 
 ;;; vol.lisp ends here
